0d0ad19b99d5a5b4402f2951518ed154776a9d85
chillandgrill <jackhu242@gmail.com>
2018-01-09 16:23:06 -0700

    Fixed bug in case of empty file.

--- src/encrypter/Encrypt.java
+++ src/encrypter/Encrypt.java
@@ -7,96 +7,102 @@ import java.io.PrintWriter;
 
 public class Encrypt {
 
-	private static StringBuilder writeToFile = new StringBuilder();
-	private static char[][] seed = new char[128][2];
+    private static StringBuilder writeToFile = new StringBuilder();
+    private static char[][] seed = new char[128][2];
 
-	private static boolean inSeed(char[] letterSequence) { // Make sure the generated two-character replacement for a
-															// single character does not already represent another
-															// character.
+    private static boolean inSeed(char[] letterSequence) { // Make sure the generated two-character replacement for a
+        // single character does not already represent another
+        // character.
 
-		for (int x = 0; x < 128; x++)
-			if (Arrays.equals(letterSequence, seed[x]))
-				return true;
-		return false;
+        for (int x = 0; x < 128; x++)
+            if (Arrays.equals(letterSequence, seed[x]))
+                return true;
+        return false;
 
-	}
+    }
 
-	private static void generateSeed() { // Generate a seed for encryption.
-		char y1 = 0;
-		char y2 = 0;
+    private static void generateSeed() { // Generate a seed for encryption.
+        char y1 = 0;
+        char y2 = 0;
+        int randomCharId = 0;
 
-		char[] seedPopulateAttempt = new char[2];
-		
-		for (int xIndex = 0; xIndex < 128; xIndex++) {
-			do {
-				for(int yIndex = 0; yIndex < 2; yIndex++)
-					seedPopulateAttempt[yIndex] = (char)(Math.random()*127);
-				}while(inSeed(seedPopulateAttempt));
-			
-			y1 = seedPopulateAttempt[0];
-			y2 = seedPopulateAttempt[1];
-			seed[xIndex][0] = y1;
-			seed[xIndex][1] = y2;
-			}
-	}
+        char[] seedPopulateAttempt = new char[2];
 
-	private static void constructFile(File inFile) {
-		
-		int offsetSeed = (int) (Math.random() * 128);
-		
-		generateSeed();
-		
-		StringBuilder origFile = new StringBuilder();
-		String temp = null;
+        for (int xIndex = 0; xIndex < 128; xIndex++) {
+            do {
+                for (int yIndex = 0; yIndex < 2; yIndex++) {
 
-		int currentCharId = 0;
+                    randomCharId = (int) (Math.random() * 128);
+                    if (randomCharId != 8)
+                        seedPopulateAttempt[yIndex] = (char) randomCharId;
+                }
+            } while (inSeed(seedPopulateAttempt));
 
-		try {
-			Scanner input = new Scanner(inFile);
-			input.useDelimiter("");
-			while (input.hasNext()) {
-				origFile.append(input.next());
-			}
+            y1 = seedPopulateAttempt[0];
+            y2 = seedPopulateAttempt[1];
+            seed[xIndex][0] = y1;
+            seed[xIndex][1] = y2;
+        }
+    }
 
-			input.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+    private static void constructFile(File inFile) {
 
-		writeToFile.append("#encrypted ");
-		writeToFile.append((char) offsetSeed);
+        int offsetSeed = (int) (Math.random() * 128);
 
-		for (int xIndex = 0; xIndex < 128; xIndex++) {
-			temp = new String(seed[xIndex]);
-			writeToFile.append(temp);
-		}
+        generateSeed();
 
-		for (int posInFile = 0; posInFile < origFile.length(); posInFile++) {
-			currentCharId = (int) origFile.charAt(posInFile);
-			if ((currentCharId + offsetSeed) >= 128) 
-				temp = new String(seed[currentCharId + offsetSeed - 128]); // MAY NEED TWEAKING
-			else 
-				temp = new String(seed[currentCharId + offsetSeed]);
-			writeToFile.append(temp);
-		}
+        StringBuilder origFile = new StringBuilder();
+        String temp = null;
 
-	}
+        int currentCharId = 0;
 
-	public static void write(File outFile) {
-		
-		constructFile(outFile);
-		
-		try {
-			PrintWriter writer = new PrintWriter(outFile);
+        try {
+            Scanner input = new Scanner(inFile);
+            input.useDelimiter("");
+            while (input.hasNext()) {
+                origFile.append(input.next());
+            }
 
-			writer.print(writeToFile.toString());
+            input.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-			writer.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		// Clear the stringbuilder for subsequent files.
-		writeToFile.delete(0, writeToFile.length());
-	}
+        writeToFile.append("#encrypted ");
+        writeToFile.append((char) offsetSeed);
+
+        for (int xIndex = 0; xIndex < 128; xIndex++) {
+            temp = new String(seed[xIndex]);
+            writeToFile.append(temp);
+            System.out.println(temp);
+        }
+
+        for (int posInFile = 0; posInFile < origFile.length(); posInFile++) {
+            currentCharId = (int) origFile.charAt(posInFile);
+            if ((currentCharId + offsetSeed) >= 128)
+                temp = new String(seed[currentCharId + offsetSeed - 128]); // MAY NEED TWEAKING
+            else
+                temp = new String(seed[currentCharId + offsetSeed]);
+            writeToFile.append(temp);
+        }
+
+    }
+
+    public static void write(File outFile) {
+
+        constructFile(outFile);
+
+        try {
+            PrintWriter writer = new PrintWriter(outFile);
+
+            writer.print(writeToFile.toString());
+
+            writer.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        // Clear the stringbuilder for subsequent files.
+        writeToFile.delete(0, writeToFile.length());
+    }
 
 }
--- src/encrypter/EncryptDecryptHub.java
+++ src/encrypter/EncryptDecryptHub.java
@@ -1,5 +1,6 @@
 package encrypter;
 
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 import java.io.File;
 
@@ -13,12 +14,18 @@ public class EncryptDecryptHub {
 
 		for(File fileName : listOfFiles) {
 			System.out.println(fileName);
-			if(fileName.getName().endsWith(".txt")){
+			if(fileName.getName().endsWith("txt")){
 				reader = new Scanner(fileName);
-				if(reader.next().equals("#encrypted"))
-					Decrypt.write(fileName);
-				else
-					Encrypt.write(fileName);
+
+				try {
+					if (reader.next().equals("#encrypted"))
+						Decrypt.write(fileName);
+					else
+						Encrypt.write(fileName);
+					reader.close();
+				}catch(NoSuchElementException e){
+					System.err.println("One or more of your files are empty!");
+				}
 			}
 		}
 	}
